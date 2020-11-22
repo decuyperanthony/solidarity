20f110badb3b07628161968bba5cf1c30f250918
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-22 22:17:52 +0100

    update read me

--- server/app/db_connection.js
+++ server/app/db_connection.js
@@ -1,7 +1,7 @@
 // Require le module PG
 const { Client } = require('pg');
 // Creer un client
-const client = new Client(process.env.PG_URL);
+const client = new Client(process.env.PG_URL_DEV);
 // Connect le client
 client.connect();
 // J'exporte le module
